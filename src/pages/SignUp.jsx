88880689fa4fd678c38629e29c51223b4820587a
Naominm <naomimbugua536@gmail.com>
2025-04-10 22:21:21 +0300

    Build form section

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,6 +1,8 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography,FormControl, TextField,Paper, FormLabel, Button } from "@mui/material";
 import BgImage from "../assets/teal.jpg";
 import Icon from "../components/icon/Icon";
+import { Label } from "@mui/icons-material";
+import { Link } from "react-router-dom";
 function SignupPage() {
   return (
     <Box
@@ -8,26 +10,25 @@ function SignupPage() {
         display: "flex",
         alignItems: "center",
         height: "100vh",
-        justifyContent: "space-between",
+        gap:"4rem",
       }}
     >
+        <Box sx={{ width:"50%", height:"100%"}}>
       <Box
         component="img"
         src={BgImage}
         sx={{
-          width: "50%",
+          width: "100%",
           height: "100%",
           objectFit: "cover",
-          position: "absolute",
-          top: 0,
-          left: 0,
+           position:"relative"
         }}
       />
       <Box
         sx={{
           height: "100%",
           width: "50%",
-          backgroundColor: "rgba(0, 128, 128, 0.8)",
+          backgroundColor: "rgba(2, 50, 50, 0.8)",
           zIndex: 5,
           position: "absolute",
           top: 0,
@@ -68,6 +69,88 @@ function SignupPage() {
           </Typography>
         </Box>
       </Box>
+      </Box>
+      <Box component="form" sx={{width:"40%", px:2}}>
+      <Paper elevation={1}sx={{display:"flex" ,
+        flexDirection:"column",width:"100%",height:"100%",mt:3}}>
+       <Box mx={2} my={2}  sx={{fontSize:"1rem"}}><Icon/></Box> 
+       <Typography variant="h5" sx={{textAlign:'center', fontWeight:700, fontFamily:"Montserrat", mb:2}}>Sign Up</Typography>
+        
+        <FormControl sx={{display:"flex",gap:"0.8rem", px:"5rem"}}>
+        <TextField
+         label="first Name"
+         type="first Name"
+         name="first Name"
+         autoComplete="first Name"
+         autoFocus
+         required
+         fullWidth
+         size="small"
+         variant="outlined"
+         sx={{fontSize:"1rem"}}
+         />
+          <TextField
+          label="last Name"
+          type="name"
+          name="last Name"
+          required
+          fullWidth
+          variant="outlined"
+          size="small"
+          />
+           <TextField
+         label="email"
+         type="email"
+         name="email"
+         autoComplete="email"
+         autoFocus
+         required
+         fullWidth
+         size="small"
+         variant="outlined"
+         />
+            <TextField
+         label="user Name"
+         type="name"
+         name="user Name"
+         autoComplete="user Name"
+         autoFocus
+         required
+         fullWidth
+         size="small"
+         variant="outlined"
+         />
+        <TextField
+         label="password"
+         type="password"
+         name="password"
+         autoComplete="password"
+         autoFocus
+         required
+         fullWidth
+         size="small"
+         variant="outlined"
+         />
+        <TextField
+         label="confirm Password"
+         type="confirm Password"
+         name="confirm Password"
+         autoComplete="confirm Password"
+         autoFocus
+         required
+         fullWidth
+         size="small"
+         variant="outlined"
+         />
+         <Button variant="contained" sx={{mt:2}}>Create An Account</Button>
+        </FormControl> 
+     <Box sx={{mt:2,mb:3, textAlign:'center'}}>   <Typography   variant="subtitle"><span>Already Have an account</span></Typography><Box  
+     sx={{pl:1}}    
+     component={Link}>Login</Box> </Box>
+        </Paper>
+      </Box>
+      
+     
     </Box>
   );
 }
