7276377e7bd40d56cf74328d99b55c90d4f8d1ee
Naominm <naomimbugua536@gmail.com>
2025-04-10 07:01:23 +0300

    Build the nav container with oa icon

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,17 +1,40 @@
-import { Typography } from "@mui/material";
+import { Typography,AppBar,Box,Toolbar,IconButton,Container } from "@mui/material";
+import BookIcon from '@mui/icons-material/Book';
 function LandingPage() {
   return (
     <div className="landingPage">
-      <Typography
-        variant="h3"
-        sx={{ textAlign: "center" }}
-        fontWeight={700}
-        mt={4}
-      >
-        Hello welcome to blogIt
-      </Typography>
+   <NavBar/>
     </div>
   );
 }
+ function NavBar(){
+  return(
+    <AppBar position=" static">
+    <Container maxWidth="xl">
+    <Toolbar>
+      <BookIcon sx={{display:{xs:"none", md:"flex"},mr:1}}/>
+      <Typography 
+      variant="h6"
+      noWrap
+      component="a"
+      sx={{
+        mr:2,
+        display:{xs:"none" , md:"flex"},
+        fontWeight:700,
+        letterSpacing:".3rem",
+        color:"inherit",
+        textDecoration:"none"
+      }}
+      >
+      BlogIt
+      </Typography>
+
+    </Toolbar>
+
+    </Container>
+
+    </AppBar>
+  )
+ }
 
 export default LandingPage;
