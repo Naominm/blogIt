abb2da7ccaca1d8e9835c31a689d572954d32edc
Naominm <naomimbugua536@gmail.com>
2025-04-10 23:17:09 +0300

    Add conditional rendering to the signup page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,156 +1,252 @@
-import { Box, Typography,FormControl, TextField,Paper, FormLabel, Button } from "@mui/material";
+import { useState } from "react";
+import {
+  Box,
+  Typography,
+  FormControl,
+  TextField,
+  Paper,
+  Button,
+} from "@mui/material";
 import BgImage from "../assets/teal.jpg";
 import Icon from "../components/icon/Icon";
-import { Label } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 function SignupPage() {
+  const [isSignup, setIsSignup] = useState(true);
   return (
     <Box
       sx={{
         display: "flex",
         alignItems: "center",
         height: "100vh",
-        gap:"4rem",
+        gap: "4rem",
       }}
     >
-        <Box sx={{ width:"50%", height:"100%"}}>
-      <Box
-        component="img"
-        src={BgImage}
-        sx={{
-          width: "100%",
-          height: "100%",
-          objectFit: "cover",
-           position:"relative"
-        }}
-      />
-      <Box
-        sx={{
-          height: "100%",
-          width: "50%",
-          backgroundColor: "rgba(2, 50, 50, 0.8)",
-          zIndex: 5,
-          position: "absolute",
-          top: 0,
-          left: 0,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "space-around",
-          alignItems: "center",
-        }}
-      >
+      <Box sx={{ width: "50%", height: "100%" }}>
+        <Box
+          component="img"
+          src={BgImage}
+          sx={{
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+            position: "relative",
+          }}
+        />
         <Box
           sx={{
-            position: "fixed",
+            height: "100%",
+            width: "50%",
+            backgroundColor: "rgba(2, 50, 50, 0.8)",
+            zIndex: 5,
+            position: "absolute",
             top: 0,
             left: 0,
-            my: 2,
-            mx: 2,
-            color: "white",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-around",
+            alignItems: "center",
           }}
         >
-          {" "}
-          <Icon />
+          <Box
+            sx={{
+              position: "fixed",
+              top: 0,
+              left: 0,
+              my: 2,
+              mx: 2,
+              color: "white",
+            }}
+          >
+            {" "}
+            <Icon />
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+            }}
+          >
+            <Typography
+              variant="h3"
+              textAlign="center"
+              fontWeight="700"
+              fontSize="2.5rem"
+              fontFamily="Montserrat"
+              color="white"
+            >
+              Join a community of writers and Readers.
+            </Typography>
+          </Box>
         </Box>
-        <Box
+      </Box>
+      <Box component="form" sx={{ width: "40%", px: 2 }}>
+        <Paper
+          elevation={1}
           sx={{
             display: "flex",
+            flexDirection: "column",
+            width: "100%",
+            height: "100%",
+            mt: 3,
           }}
         >
-          <Typography
-            variant="h3"
-            textAlign="center"
-            fontWeight="700"
-            fontSize="3rem"
-            fontFamily="Montserrat"
-            color="white"
+          <Box
+            mx={2}
+            my={2}
+            sx={{ fontSize: "1rem", fontFamily: "Montserrat" }}
           >
-            Join a community of writers and Readers.
+            <Icon />
+          </Box>
+          <Typography
+            variant="h5"
+            sx={{
+              textAlign: "center",
+              fontWeight: 700,
+              fontFamily: "Montserrat",
+              mb: 2,
+            }}
+          >
+            S {isSignup ? "Sign Up" : "Login"}
           </Typography>
-        </Box>
-      </Box>
-      </Box>
-      <Box component="form" sx={{width:"40%", px:2}}>
-      <Paper elevation={1}sx={{display:"flex" ,
-        flexDirection:"column",width:"100%",height:"100%",mt:3}}>
-       <Box mx={2} my={2}  sx={{fontSize:"1rem"}}><Icon/></Box> 
-       <Typography variant="h5" sx={{textAlign:'center', fontWeight:700, fontFamily:"Montserrat", mb:2}}>Sign Up</Typography>
-        
-        <FormControl sx={{display:"flex",gap:"0.8rem", px:"5rem"}}>
-        <TextField
-         label="first Name"
-         type="first Name"
-         name="first Name"
-         autoComplete="first Name"
-         autoFocus
-         required
-         fullWidth
-         size="small"
-         variant="outlined"
-         sx={{fontSize:"1rem"}}
-         />
-          <TextField
-          label="last Name"
-          type="name"
-          name="last Name"
-          required
-          fullWidth
-          variant="outlined"
-          size="small"
-          />
-           <TextField
-         label="email"
-         type="email"
-         name="email"
-         autoComplete="email"
-         autoFocus
-         required
-         fullWidth
-         size="small"
-         variant="outlined"
-         />
-            <TextField
-         label="user Name"
-         type="name"
-         name="user Name"
-         autoComplete="user Name"
-         autoFocus
-         required
-         fullWidth
-         size="small"
-         variant="outlined"
-         />
-        <TextField
-         label="password"
-         type="password"
-         name="password"
-         autoComplete="password"
-         autoFocus
-         required
-         fullWidth
-         size="small"
-         variant="outlined"
-         />
-        <TextField
-         label="confirm Password"
-         type="confirm Password"
-         name="confirm Password"
-         autoComplete="confirm Password"
-         autoFocus
-         required
-         fullWidth
-         size="small"
-         variant="outlined"
-         />
-         <Button variant="contained" sx={{mt:2}}>Create An Account</Button>
-        </FormControl> 
-     <Box sx={{mt:2,mb:3, textAlign:'center'}}>   <Typography   variant="subtitle"><span>Already Have an account</span></Typography><Box  
-     sx={{pl:1}}    
-     component={Link}>Login</Box> </Box>
+
+          {isSignup && (
+            <>
+              <FormControl sx={{ display: "flex", gap: "0.8rem", px: "5rem" }}>
+                <TextField
+                  label="first Name"
+                  type="first Name"
+                  name="first Name"
+                  autoComplete="first Name"
+                  autoFocus
+                  required
+                  fullWidth
+                  size="small"
+                  variant="outlined"
+                  sx={{ fontSize: "1rem" }}
+                />
+                <TextField
+                  label="last Name"
+                  type="name"
+                  name="last Name"
+                  required
+                  fullWidth
+                  variant="outlined"
+                  size="small"
+                />
+                <TextField
+                  label="email"
+                  type="email"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                  required
+                  fullWidth
+                  size="small"
+                  variant="outlined"
+                />
+                <TextField
+                  label="user Name"
+                  type="name"
+                  name="user Name"
+                  autoComplete="user Name"
+                  autoFocus
+                  required
+                  fullWidth
+                  size="small"
+                  variant="outlined"
+                />
+                <TextField
+                  label="password"
+                  type="password"
+                  name="password"
+                  autoComplete="password"
+                  autoFocus
+                  required
+                  fullWidth
+                  size="small"
+                  variant="outlined"
+                />
+                <TextField
+                  label="confirm Password"
+                  type="confirm Password"
+                  name="confirm Password"
+                  autoComplete="confirm Password"
+                  autoFocus
+                  required
+                  fullWidth
+                  size="small"
+                  variant="outlined"
+                />
+                <Button variant="contained" sx={{ mt: 2 }}>
+                  Create An Account
+                </Button>
+              </FormControl>
+            </>
+          )}
+          <Box sx={{ mt: 2, mb: 3, textAlign: "center" }}>
+            {" "}
+            <Typography variant="subtitle">
+              <span>
+                {isSignup
+                  ? "Already Have an account"
+                  : "Do not have An account"}
+              </span>
+            </Typography>
+            <Box sx={{ pl: 1 }}> {isSignup ? "Login" : "Sign Up"}</Box>{" "}
+          </Box>
         </Paper>
       </Box>
-      
-     
+
+      {!isSignup && (
+        <>
+          <Typography
+            variant="h5"
+            sx={{
+              textAlign: "center",
+              fontWeight: 700,
+              fontFamily: "Montserrat",
+              mb: 2,
+            }}
+          >
+            Login
+          </Typography>
+          <FormControl sx={{ display: "flex", gap: "0.8rem", px: "5rem" }}>
+            <TextField
+              label="UserName or Email"
+              type="text"
+              name="identifier"
+              required
+              fullWidth
+              size="small"
+              variant="outlined"
+            />
+
+            <TextField
+              label="password"
+              type="password"
+              name="password"
+              required
+              fullWidth
+              size="small"
+              variant="outlined"
+            />
+            <Button variant="contained" sx={{ mt: 2 }}>
+              Login
+            </Button>
+          </FormControl>
+          <Box sx={{ mt: 2, mb: 3, textAlign: "center" }}>
+            <Typography variant="subtitle2">
+              Do not have an Account?{" "}
+            </Typography>
+            <Box
+              component="span"
+              onClick={() => setIsSignup(!isSignup)}
+              sx={{ pl: 1, color: "blue", cursor: "pointer" }}
+            >
+              {isSignup ? "Login" : "Sign Up"}
+            </Box>
+          </Box>
+        </>
+      )}
     </Box>
   );
 }
