2f144a59cd7b8fe2d1429c0f1d1a62b152c8952a
Naominm <naomimbugua536@gmail.com>
2025-04-10 08:12:15 +0300

    Build Nav

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,5 +1,8 @@
-import { Typography,AppBar,Box,Toolbar,IconButton,Container } from "@mui/material";
+import {useState} from 'react'
+import { Typography,AppBar,Box,Toolbar,IconButton,Container,MenuItem,Menu} from "@mui/material";
 import BookIcon from '@mui/icons-material/Book';
+import MenuIcon from '@mui/icons-material/Menu';
+import {Link} from "react-router-dom"
 function LandingPage() {
   return (
     <div className="landingPage">
@@ -8,6 +11,14 @@ function LandingPage() {
   );
 }
  function NavBar(){
+  const [anchorEl, setAnchorEl] = useState(null);
+  const isMenuOpen = Boolean(anchorEl);
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
   return(
     <AppBar position=" static">
     <Container maxWidth="xl">
@@ -16,7 +27,8 @@ function LandingPage() {
       <Typography 
       variant="h6"
       noWrap
-      component="a"
+      component={Link}
+      to='/'
       sx={{
         mr:2,
         display:{xs:"none" , md:"flex"},
@@ -28,7 +40,52 @@ function LandingPage() {
       >
       BlogIt
       </Typography>
+    <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex", justifyContent:"right" } }}>
+    <MenuItem component={Link} to="/" sx={{ color: "white" }}>About</MenuItem>
+    <MenuItem component={Link} to="/" sx={{ color: "white" }}>Account</MenuItem>
+  </Box>
+    <Box
+    sx={{flexGrow:1 , display:{xs:"flex" , md:"none "}}} >
+      <IconButton
+      size="large"
+      aria-label="Account of the current user"
+      aria-controls="menu-appbar"
+      aria-haspopup="true"
+      color="inherit"
+      onClick={handleMenuOpen}
+      >
+        <MenuIcon/>
+      </IconButton>
+    <Menu 
+    id='menu-appbar'
+    anchorEl={anchorEl}
+    anchorOrigin={{
+      vertical:"bottom",
+      horizontal:"left"
+    }}
+    keepMounted
+    transformOrigin={{
+      vertical:"top",
+      horizontal:"left"
+    }}
+    open={isMenuOpen}
 
+    onClose={handleMenuClose}
+   sx={{
+    display:{xs:"block", md:"none"},
+   }}
+   PaperProps={{
+    sx:{
+      backgroundColor:"white",
+      color:"black"
+    }
+   }}
+   >
+    
+ <MenuItem component={Link} to="/" sx={{ color: "black" }}>About</MenuItem>
+ <MenuItem component={Link} to="/"  sx={{ color: "black" }}>Account</MenuItem>
+ </Menu>
+    </Box>
     </Toolbar>
 
     </Container>
