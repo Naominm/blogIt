9e82daaad4033e9a0e8be4f6b4da48ff6ee6d2c6
Naominm <naomimbugua536@gmail.com>
2025-04-13 12:05:05 +0300

    Populating the nav on writers page

--- src/pages/WritePage.jsx
+++ src/pages/WritePage.jsx
@@ -1,10 +1,35 @@
-import { Typography } from "@mui/material";
+import { Typography, Avatar, Box } from "@mui/material";
 import NavBar from "../components/NavBar";
+import Icon from "../components/icon/Icon";
 function WritersPage() {
   return (
     <>
-      <NavBar />
-      <Typography variant="h4"> Welcome to the writers page</Typography>
+      <NavBar
+        icon={Icon}
+        menuItems={[
+          { label: "listing", path: "/explore" },
+          { label: "Write", path: "/writers" },
+          { label: "My Blogs", path: "/my-blogs" },
+          { label: "My Profile", path: "/profile" },
+        ]}
+        extraComponents={
+          <>
+            <Avatar alt="Naomi" src="/avatar.png" />
+          </>
+        }
+      />
+      <Typography variant="h6">This is the writers page</Typography>
+      <Box component="div">
+        <WritersForm />
+      </Box>
+    </>
+  );
+}
+
+function WritersForm() {
+  return (
+    <>
+      <Typography variant="h6">The form goes here</Typography>
     </>
   );
 }
