12ddef2a0996da794a3b0ca1d89c7d1dfc10082a
Naominm <naomimbugua536@gmail.com>
2025-04-13 14:33:33 +0300

    import the nav component

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -1,9 +1,34 @@
-import { Typography } from "@mui/material";
+import { Typography, Card, Paper, Box } from "@mui/material";
+import NavBar from "../components/NavBar";
 
 function ArticlesPage() {
   return (
     <>
-      <Typography variant="h4">Hello this is the Articles Page</Typography>
+      <ArticlesContent />
+    </>
+  );
+}
+
+function ArticlesContent() {
+  return (
+    <>
+      <NavBar />
+      <Box
+        component="div"
+        sx={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Paper elevation={0} sx={{ mt: 5, width: "60%", height: "100%" }}>
+          <Typography variant="h3" sx={{ fontWeight: 700 }}>
+            Want to speak English like a native speaker? Read this first.
+          </Typography>
+        </Paper>
+      </Box>
     </>
   );
 }
