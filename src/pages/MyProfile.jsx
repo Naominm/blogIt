bf153f94a4d6a20c0bc954830ce8ef6fb15d545d
Naominm <naomimbugua536@gmail.com>
2025-04-13 21:59:17 +0300

    Add nav component to the profile page

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,7 +1,24 @@
+import { Avatar } from "@mui/material";
+import Icon from "../components/icon/Icon";
+import NavBar from "../components/NavBar";
 import { Box, Typography } from "@mui/material";
 function MyProfilePage() {
   return (
     <Box component="div">
+      <NavBar
+        icon={Icon}
+        menuItems={[
+          { label: "listing", path: "/explore" },
+          { label: "Write", path: "/writers" },
+          { label: "My Blogs", path: "/my-blogs" },
+          { label: "My Profile", path: "/profile" },
+        ]}
+        extraComponents={
+          <>
+            <Avatar alt="Naomi" src="/avatar.png" />
+          </>
+        }
+      />
       <Typography variant="h6"> Welcome to your profile</Typography>
     </Box>
   );
