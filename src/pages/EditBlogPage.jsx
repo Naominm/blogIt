907bdfb2b4f9b65e7def054ad23386ea22782d04
Naominm <naomimbugua536@gmail.com>
2025-04-13 18:47:29 +0300

    Add nav to the Edit Blog page

--- src/pages/EditBlogPage.jsx
+++ src/pages/EditBlogPage.jsx
@@ -1,9 +1,24 @@
-import { Typography } from "@mui/material";
-
+import { Avatar } from "@mui/material";
+import AvatarImage from "../assets/blog.png";
+import NavBar from "../components/NavBar";
+import Icon from "../components/icon/Icon";
 function EditBlogPAge() {
   return (
     <>
-      <Typography>This is the Edit blog page</Typography>
+      <NavBar
+        icon={Icon}
+        menuItems={[
+          { label: "listing", path: "/explore" },
+          { label: "Write", path: "/writers" },
+          { label: "My Blogs", path: "/my-blogs" },
+          { label: "My Profile", path: "/profile" },
+        ]}
+        extraComponents={
+          <>
+            <Avatar alt="Naomi" src="/avatar.png" />
+          </>
+        }
+      />
     </>
   );
 }
