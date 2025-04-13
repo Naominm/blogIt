eee789b6609b90033a937c1842011819600a90d9
Naominm <naomimbugua536@gmail.com>
2025-04-13 16:31:04 +0300

    Import the navbar to the articles page and populate it

--- src/pages/MyBlogs.jsx
+++ src/pages/MyBlogs.jsx
@@ -1,8 +1,24 @@
-import { Typography } from "@mui/material";
+import { Typography, Avatar } from "@mui/material";
+import NavBar from "../components/NavBar";
+import Icon from "../components/icon/Icon";
 
 function MyBlogsPage() {
   return (
     <>
+      <NavBar
+        icon={Icon}
+        menuItems={[
+          { label: "listing", path: "/explore" },
+          { label: "Write", path: "/writers" },
+          { label: "My Blogs", path: "/my-blogs" },
+          { label: "My Profile", path: "/profile" },
+        ]}
+        extraComponents={
+          <>
+            <Avatar alt="Naomi" src="/avatar.png" />
+          </>
+        }
+      />
       <Typography variant="h6">All the blogs that you have authored</Typography>
     </>
   );
