28e05d70af6d4e40691409c965539d3967189bdd
Naominm <naomimbugua536@gmail.com>
2025-04-13 18:59:02 +0300

    Make writersCardForm a component and import the card to the EditBlogPage

---
+++ src/components/WritersCard.jsx
@@ -0,0 +1,74 @@
+import { TextField, Box, Paper, Button, IconButton } from "@mui/material";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import CloseIcon from "@mui/icons-material/Close";
+
+export default function WritersForm() {
+  return (
+    <>
+      <Box
+        sx={{
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: "2rem",
+        }}
+      >
+        <Paper
+          component="form"
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 2,
+            width: { xs: "100%", md: "50%" },
+            height: "100%",
+            mt: 3,
+            pb: 2,
+          }}
+        >
+          <Box
+            sx={{
+              position: "relative",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "40%",
+              backgroundColor: "#F6F8FA",
+            }}
+          >
+            <Button variant="contained">
+              <CloudUploadIcon />
+            </Button>
+            <IconButton
+              sx={{
+                position: "absolute",
+                top: 10,
+                right: 10,
+                bgcolor: "white",
+                color: "black",
+              }}
+            >
+              <CloseIcon />
+            </IconButton>
+          </Box>
+          <TextField
+            fullWidth
+            label="Enter Your title"
+            sx={{ backgroundColor: "#F6F8FA" }}
+          />
+          <TextField
+            label="Blog Content"
+            multiline
+            rows={8}
+            variant="outlined"
+            fullWidth
+            sx={{ backgroundColor: "#F6F8FA" }}
+          />
+          <Button variant="contained" sx={{ my: 1, mx: 1 }}>
+            submit
+          </Button>
+        </Paper>
+      </Box>
+    </>
+  );
+}
