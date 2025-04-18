fae8d464240b37e715acac035bbe6f01b51789c7
Naominm <naomimbugua536@gmail.com>
2025-04-18 05:38:02 +0300

    persist the state

--- store/userProfileStore.js
+++ store/userProfileStore.js
@@ -1,20 +1,28 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const useProfileStore = create((set) => ({
-  avatarUrl: "",
-  setAvatarUrl: (url) => set({ avatarUrl: url }),
+const useProfileStore = create(
+  persist(
+    (set) => ({
+      avatarUrl: "",
+      setAvatarUrl: (url) => set({ avatarUrl: url }),
 
-  phoneNumber: "",
-  setPhoneNumber: (phone) => set({ phoneNumber: phone }),
+      phoneNumber: "",
+      setPhoneNumber: (phone) => set({ phoneNumber: phone }),
 
-  occupation: "",
-  setOccupation: (occ) => set({ occupation: occ }),
+      occupation: "",
+      setOccupation: (occ) => set({ occupation: occ }),
 
-  bio: "",
-  setBio: (b) => set({ bio: b }),
+      bio: "",
+      setBio: (b) => set({ bio: b }),
 
-  secondaryEmail: "",
-  setSecondaryEmail: (email) => set({ secondaryEmail: email }),
-}));
+      secondaryEmail: "",
+      setSecondaryEmail: (email) => set({ secondaryEmail: email }),
+    }),
+    {
+      name: "profile-storage",
+    },
+  ),
+);
 
 export default useProfileStore;
